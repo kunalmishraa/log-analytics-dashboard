ab81f171820c732efe4a61ee7051984647ee4e4e
Kunal Mishra <kunal.mishra@timesinternet.in>
2025-11-09 22:47:40 +0530

    added the project for scalable purpose

---
+++ src/main/java/com/kunal/loganalytics/LogAnalyticsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.loganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/loganalytics/LogAnalyticsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.loganalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
